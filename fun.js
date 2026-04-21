f0a2640e2731dbd4c4fccc5ef0be04e2007146ba
Юля <tsigelnik.yulia@gmail.com>
2026-04-21 13:48:30 +1000

    fix and update

--- fun.js
+++ fun.js
@@ -20,38 +20,40 @@ function button() {
     document.getElementById('square3').classList.remove('selected');
 
     var d = document.getElementById('dejgi').innerHTML;
-    d = Number(d);
-    d += 1;
+    if (isPlay === false) {
+        d = Number(d);
+        d += 1;
+        isPlay = true;
+    }
     document.getElementById('dejgi').innerHTML = d;
     startNewGame()
 }
 
-var choise = false; 
-var isWin;
+var isPlay = false;
+var choise = false;
 var numUserChoise;
 var position; 
 var gamestatus; 
 var vic_pos;
-var opensquare;
 
 function startNewGame(){ 
-
     choise = true;
     numUserChoise = null;
-
     vic_pos = Math.floor(Math.random()*3) + 1; 
     gamestatus = 'choise';
-
     document.getElementById('button').innerHTML = '<h1>Now playing...</h1>'
-
 }
 
 function square(number){
     var square = 'square' + number;
     var s1 = document.getElementById('s1').innerHTML;
     var s2 = document.getElementById('s2').innerHTML;
+    var c1 = document.getElementById('c1').innerHTML;
+    var c2 = document.getElementById('c2').innerHTML;
     s1 = Number(s1);
     s2 = Number(s2);
+    c1 = Number(c1);
+    c2 = Number(c2);
 
     
     if (gamestatus === 'choise'){
@@ -59,7 +61,7 @@ function square(number){
         numUserChoise = number;
         document.getElementById('square' + numUserChoise).classList.add('selected');
 
-        opensquare = openSquare();
+        var opensquare = openSquare();
         var goatSquare = document.getElementById('square' + opensquare);
         goatSquare.style.backgroundColor = '#eb6262';
         goatSquare.onclick = null; 
@@ -69,7 +71,6 @@ function square(number){
 
 
     else if (gamestatus === 'offer'){
-
         if (number === numUserChoise){
             choise =  false;
             position = numUserChoise;
@@ -86,16 +87,25 @@ function square(number){
         var isWin = (position === vic_pos)
 
         if (isWin){
-            s1 ++;
-            document.getElementById('s1').innerHTML = s1;
+            if (choise){
+                s1++;
+                document.getElementById('s1').innerHTML = s1;
+            }else{
+                c1++;
+                document.getElementById('c1').innerHTML = c1;
+            }
         } else {
-            s2 ++;
-            document.getElementById('s2').innerHTML = s2;
+            if (choise){
+                s2++;
+                document.getElementById('s2').innerHTML = s2;
+            }else{
+                c2++;
+                document.getElementById('c2').innerHTML = c2;
+            }
         }
         endGame()
     }
 }
-
 function openSquare(){
 
     var possibleGoats = [];
@@ -110,11 +120,9 @@ function openSquare(){
         return possibleGoats[randomidx];
     }
 }
-
 function endGame(){
     for (var i = 1; i <= 3; i++){
         var sq = document.getElementById('square' + i);
-
         if (vic_pos === i){
             sq.style.backgroundColor = '#72eea6';
         }
@@ -125,5 +133,6 @@ function endGame(){
     square1.onclick = null;
     square2.onclick = null;
     square3.onclick = null;
+    isPlay = false;
     document.getElementById('button').innerHTML = '<h1>Сlick to start!</h1>'
 }
